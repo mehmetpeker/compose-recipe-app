dade3aa5865443173c4e52cd1cac4bb406ad8738
mehmetpeker <mehmetpeker41@gmail.com>
2024-01-05 21:48:06 +0300

    reset password service implemented

--- app/src/main/java/com/mehmetpeker/recipe/presentation/authentication/resetPassword/ResetPasswordModule.kt
+++ app/src/main/java/com/mehmetpeker/recipe/presentation/authentication/resetPassword/ResetPasswordModule.kt
@@ -4,5 +4,5 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val resetPasswordModule = module {
-    viewModel { ResetPasswordViewModel() }
+    viewModel { ResetPasswordViewModel(get(), get()) }
 }
